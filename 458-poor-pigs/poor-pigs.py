73c296d90d43fe2282753e5a8bb276f33ea7e125
Uday Bhatia <101352755+BhatiaUday@users.noreply.github.com>
2024-03-26 01:11:31 +0530

    Time: 33 ms (65.52%) | Memory: 16.5 MB (52.41%) - LeetSync

--- 458-poor-pigs/poor-pigs.py
+++ 458-poor-pigs/poor-pigs.py
@@ -1,7 +1,7 @@
 class Solution:
     def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
         i=0
-        while (minutesToTest//minutesToDie +1)**i<buckets:
+        while buckets> (minutesToTest//minutesToDie +1)**i:
             i+=1
         return(i)
 
