42ddeae542ffb4884f6bd773a8154a2e2544fddc
Uday Bhatia <101352755+BhatiaUday@users.noreply.github.com>
2024-03-26 22:06:05 +0530

    Time: 45 ms (8.58%) | Memory: 16.5 MB (57.89%) - LeetSync

---
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
@@ -0,0 +1,15 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        x=0
+        y=1
+        bal=0
+        for i in range(n):
+            if i%7==0:
+                x+=1
+                y=x
+                bal+=y
+                y+=1
+            else:
+                bal+=y
+                y+=1
+        return bal
