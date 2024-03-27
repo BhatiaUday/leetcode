b8ec044ce4a9d343e4dcb86de270448444b461f4
Uday Bhatia <101352755+BhatiaUday@users.noreply.github.com>
2024-03-27 22:35:38 +0530

    Time: 40 ms (81.61%) | Memory: 21.9 MB (8.08%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,10 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        st=""
-        st1=""
-        for i in s:
-            if i in "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890":
-                st1+=i
-                st=i+st
-        return st.lower()==st1.lower()
+        st="".join([ch.lower() for ch in s if ch.isalnum()])
+        return st==st[::-1]
         
