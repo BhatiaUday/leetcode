b954b5c5ca15c83bc35c36157728ca4ea965261a
Uday Bhatia <101352755+BhatiaUday@users.noreply.github.com>
2024-03-27 21:53:57 +0530

    Time: 30 ms (87.63%) | Memory: 16.7 MB (6.03%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,3 +1,16 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        def dfs(x, n):
+            if n == 0:
+                return 1
+            if n == 1:
+                return x
+    
+            if n%2 == 1:
+                return x* dfs(x*x, n//2)
+            else:
+                return dfs(x*x, n//2)
+        if n > 0:
+            return dfs(x,n)
+        else:
+            return 1/dfs(x,-n)
