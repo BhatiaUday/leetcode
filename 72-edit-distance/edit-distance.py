be98da54fb6585771b0783ec3ac51be19e8affad
Uday Bhatia <101352755+BhatiaUday@users.noreply.github.com>
2024-03-31 18:11:21 +0530

    Time: 360 ms (5.02%) | Memory: 38.1 MB (5.01%) - LeetSync

---
+++ 72-edit-distance/edit-distance.py
@@ -0,0 +1,35 @@
+import numpy
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        token1=word1
+        token2=word2
+        distances = numpy.zeros((len(token1) + 1, len(token2) + 1))
+
+        for t1 in range(len(token1) + 1):
+            distances[t1][0] = t1
+
+        for t2 in range(len(token2) + 1):
+            distances[0][t2] = t2
+            
+        a = 0
+        b = 0
+        c = 0
+        
+        for t1 in range(1, len(token1) + 1):
+            for t2 in range(1, len(token2) + 1):
+                if (token1[t1-1] == token2[t2-1]):
+                    distances[t1][t2] = distances[t1 - 1][t2 - 1]
+                else:
+                    a = distances[t1][t2 - 1]
+                    b = distances[t1 - 1][t2]
+                    c = distances[t1 - 1][t2 - 1]
+                    
+                    if (a <= b and a <= c):
+                        distances[t1][t2] = a + 1
+                    elif (b <= a and b <= c):
+                        distances[t1][t2] = b + 1
+                    else:
+                        distances[t1][t2] = c + 1
+
+        # printDistances(distances, len(token1), len(token2))
+        return int(distances[len(token1)][len(token2)])
